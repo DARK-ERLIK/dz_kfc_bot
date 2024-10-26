4cbe6a151a55269e3fd4741b2cd4a81f5c84606a
Ravshan ISHIMOV <remcorps2@gmail.com>
2024-10-26 16:57:25 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,54 @@
+import telebot
+import buttons as bt
+import database as db
+from geopy import Photon
+
+geolocator = Photon(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0")
+
+bot =telebot.TeleBot(token="7880901995:AAEyWZDyn3-3YUz_9Y3yW-untNYIZzPCvxM")
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_id = message.from_user.id
+    bot.send_message(user_id, "Добро пожаловать в бот доставки!")
+    checker = db.check_user(user_id)
+    if checker == True:
+        bot.send_message(user_id, "Главное меню")
+    elif checker == False:
+        bot.send_message(user_id, "Введите своё имя для регистрации")
+        print(message.text)
+        bot.register_next_step_handler(message, get_name)
+
+def get_name(message):
+    user_id = message.from_user.id
+    name = message.text
+    print(message.text)
+    bot.send_message(user_id, "Теперь поделитесь своим номером", reply_markup=bt.phone_buttons())
+    bot.register_next_step_handler(message, get_phone_number, name)
+
+def get_phone_number(message, name):
+    user_id = message.from_user.id
+    if message.contact:
+        phone_number = message.contact.phone_number
+        print(phone_number)
+        bot.send_message(user_id, "Отправьте свою локацию", reply_markup=bt.location_button())
+        bot.register_next_step_handler(message, get_location, name, phone_number)
+    else:
+        bot.send_message(user_id, "Отправьте свой номер через кнопку меню")
+        bot.register_next_step_handler(message, get_phone_number, name)
+
+def get_location(message, name, phone_number):
+    user_id = message.from_user.id
+    if message.location:
+        latitude = message.location.latitude
+        longitude = message.location.longitude
+        address = geolocator.reverse((latitude, longitude)).address
+        print(name, phone_number, address)
+        db.add_user(name=name, phone_number=phone_number, user_id=user_id)
+        bot.send_message(user_id, "Вы успешно зарегестрировались!")
+        bot.send_message(user_id, "Главное меню: ")
+    else:
+        bot.send_message(user_id, "Отправьте свою локацию через кнопку меню")
+        bot.register_next_step_handler(message, get_location, name, phone_number)
+
+bot.infinity_polling()
